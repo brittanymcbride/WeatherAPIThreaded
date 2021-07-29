26ca89420fb259adb5db2aca1153c719db94d73a
B. McBride <brittanymcbride@users.noreply.github.com>
2021-07-29 01:26:28 -0600

    Update WeatherAverageMaxTemp.py

--- WeatherAverageMaxTemp.py
+++ WeatherAverageMaxTemp.py
@@ -8,21 +8,24 @@ import requests
 import concurrent.futures
 import statistics
 
-def get_Average_Max_Temp(weatherUrl, timeout=10):
+#Calculate average max temperature for each API call
+def get_average_max_temp(weatherUrl, timeout=10):
     response = requests.get(url=weatherUrl, timeout=timeout)
-
     temps=response.json()
     maxtemps = [i['max_temp'] for i in temps['consolidated_weather']]
     average = statistics.mean(maxtemps)
     return temps['title'] + " Average Max Temp: " + str(round(average, 2))
 
+#MetaWeather API URLs
 urls = ['https://www.metaweather.com/api/location/2487610/', 
         'https://www.metaweather.com/api/location/2442047/', 
         'https://www.metaweather.com/api/location/2366355/']
+
+
 with concurrent.futures.ThreadPoolExecutor() as executor:
     futures = []
     for url in urls:
-        futures.append(executor.submit(get_Average_Max_Temp, weatherUrl=url))
+        futures.append(executor.submit(get_average_max_temp, weatherUrl=url))
     for future in concurrent.futures.as_completed(futures):
         try:
             data = future.result()
