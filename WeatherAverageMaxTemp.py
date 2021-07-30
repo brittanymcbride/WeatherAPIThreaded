40019a1db8044b6c94f6b0aa9fe3935c01d79d7d
B. McBride <brittanymcbride@users.noreply.github.com>
2021-07-29 22:47:01 -0600

    Update WeatherAverageMaxTemp.py

--- WeatherAverageMaxTemp.py
+++ WeatherAverageMaxTemp.py
@@ -1,20 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 28 20:58:09 2021
-
-@author: Brittany McBride
-"""
 import requests
 import concurrent.futures
 import statistics
+from json.decoder import JSONDecodeError
 
 #Calculate average max temperature for each API call
 def get_average_max_temp(weatherUrl, timeout=10):
-    response = requests.get(url=weatherUrl, timeout=timeout)
-    temps=response.json()
-    maxtemps = [i['max_temp'] for i in temps['consolidated_weather']]
-    average = statistics.mean(maxtemps)
-    return temps['title'] + " Average Max Temp: " + str(round(average, 2))
+    try:
+        response = requests.get(url=weatherUrl, timeout=timeout)
+        
+    except requests.exceptions.HTTPError as err_h:
+        return "An Http Error occurred:" + repr(err_h)
+    except requests.exceptions.ConnectionError as err_c:
+        return "An Error Connecting to the API occurred:" + repr(err_c)
+    except requests.exceptions.Timeout as err_t:
+        return "A Timeout Error occurred:" + repr(err_t)
+    except requests.exceptions.RequestException as err_:
+        return "An Unknown Error occurred" + repr(err_)
+    else:   
+        try: 
+            weather_=response.json()
+        except JSONDecodeError as e_:
+            return "An Unknown Error occurred" + repr(e_)
+        else:
+        #check that there is at least 1 day of temperatures
+         assert len(weather_['consolidated_weather']) >= 1
+         
+         maxtemps = [i['max_temp'] for i in weather_['consolidated_weather']]
+         
+         # Validate that only numbers from MaxTemps used
+         check_temps = [x for x in maxtemps if isinstance(x, (float,int))]
+         
+         #Calculate Average of Max Temperatures
+         average_ = statistics.mean(check_temps)
+        
+         
+    return weather_['title'] + " Average Max Temp: " + str(round(average_, 2))
 
 #MetaWeather API URLs
 urls = ['https://www.metaweather.com/api/location/2487610/', 
